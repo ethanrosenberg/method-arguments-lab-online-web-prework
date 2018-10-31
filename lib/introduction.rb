2511a073038594db44c3c9f25b3012b4a59e3c8c
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-10-31 08:18:54 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction(name)
 end
 
 def introduction_with_laungage(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
 end
